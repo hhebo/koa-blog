8c98dc2e16b96f98999a749f136df699ca1852ca
hhebo <tmaclimt1@gmail.com>
2017-05-17 14:43:15 +0800

    change layout files

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -19,22 +19,22 @@ const createUser = async (ctx, next) => {
   let repassword = req.body.repassword;
   try {
     if (!(name.trim().length) >= 1 && name.trim().length <= 10) {
-      ctx.flash('error','名字长度为1-10');
+      ctx.flash('error', '名字长度为1-10');
     }
     if (['m','f','x'].indexOf(gender) === -1) {
-      ctx.flash('error','性别不对');
+      ctx.flash('error', '性别不对');
     }
     if (!(bio.trim().length >= 1 && bio.trim().length <= 30)) {
-      ctx.flash('error','简介不能超过30个字符');
+      ctx.flash('error', '简介不能超过30个字符');
     }
     if (!ctx.req.file.originalname) {
-      ctx.flash('error','需要上传头像');
+      ctx.flash('error', '需要上传头像');
     }
     if (password.length < 6) {
-      ctx.flash('error','密码长度必须大于6');
+      ctx.flash('error', '密码长度必须大于6');
     }
     if (password !== repassword) {
-      ctx.flash('error','两次密码不同');
+      ctx.flash('error', '两次密码不同');
     }
     avatar = req.file.path.split('/').pop();
   } catch(e) {
@@ -55,22 +55,22 @@ const createUser = async (ctx, next) => {
     user = create.ops[0];
     user.password = null;
     ctx.session.user = user;
-    ctx.flash('success','注册成功');
+    ctx.flash('success', '注册成功');
   } catch(e) {
     fs.unlink(ctx.req.file.path);
     if (e.message.match('E11000 duplicate key')) {
-      ctx.flash('error','用户名已被占用');
+      ctx.flash('error', '用户名已被占用');
       return ctx.redirect('/users/signup');
     }
   }
-  ctx.redirect('/');
+  ctx.redirect('/posts');
 };
 
 function encrypt(plainPassword,method){
   const hash = crypto.createHash(method);
   hash.update(plainPassword);
   return hash.digest('hex');
-};
+}
 
 const signin = async (ctx, next) => {
   const locals = {
@@ -87,26 +87,26 @@ const login = async (ctx, next) => {
     ctx.flash('error', '用户不存在');
     return ctx.redirect('back');
   }
-  if(!auth(password,user.password)){
+  if (!auth(password, user.password)) {
     ctx.flash('error', '用户名或者密码错误');
     return ctx.redirect('back');
   }
   ctx.flash('success', '登录成功');
   user.password = null;
   ctx.session.user = user;
-  ctx.redirect('/');
+  ctx.redirect('/posts');
 };
 
-function auth(plainPassword,encryPassword){
+function auth(plainPassword, encryPassword){
   let hash = crypto.createHash('sha256');
   hash.update(plainPassword);
   return hash.digest('hex') === encryPassword ;
-};
+}
 
 const signout = async (ctx, next) => {
   ctx.session.user = null;
   ctx.flash('success', '登出成功');
-  ctx.redirect('/');
+  ctx.redirect('/posts');
 };
 
 export default {
--- app/routes/user.js
+++ app/routes/user.js
@@ -8,7 +8,7 @@ const router = Router({
 });
 
 const upload = multer({
-  dest: path.join(__dirname, '../../public/img')
+  dest: path.join(__dirname, '../assets/images')
 });
 
 router.get('/signup', user.newUser);
